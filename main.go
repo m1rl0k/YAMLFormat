d3b71640f1f5e32f30333e05e913d51fea76d2e3
M1RL0K <11264689+m1rl0k@users.noreply.github.com>
2023-04-01 19:57:58 -0400

    Create main.go

---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclwrite"
+	"gopkg.in/yaml.v3"
+)
+
+func main() {
+	if err := filepath.Walk(".", processFile); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func processFile(path string, info os.FileInfo, err error) error {
+	if err != nil {
+		return err
+	}
+
+	if info.IsDir() {
+		return nil
+	}
+
+	ext := filepath.Ext(path)
+	if ext == ".yaml" || ext == ".yml" {
+		return formatYAMLFile(path)
+	} else if ext == ".hcl" {
+		return formatHCLFile(path)
+	}
+
+	return nil
+}
+
+func formatYAMLFile(path string) error {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	var yamlData interface{}
+	if err := yaml.Unmarshal(data, &yamlData); err != nil {
+		return err
+	}
+
+	formattedData, err := yaml.Marshal(yamlData)
+	if err != nil {
+		return err
+	}
+
+	if !strings.EqualFold(string(data), string(formattedData)) {
+		if err := ioutil.WriteFile(path, formattedData, 0644); err != nil {
+			return err
+		}
+		fmt.Printf("Formatted YAML file: %s\n", path)
+	}
+
+	return nil
+}
+
+func formatHCLFile(path string) error {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	file, diags := hcl.Parse(string(data))
+	if diags.HasErrors() {
+		return fmt.Errorf("Error parsing HCL file '%s': %s", path, diags.Error())
+	}
+
+	formattedData := hclwrite.Format(file)
+
+	if !strings.EqualFold(string(data), string(formattedData)) {
+		if err := ioutil.WriteFile(path, formattedData, 0644); err != nil {
+			return err
+		}
+		fmt.Printf("Formatted HCL file: %s\n", path)
+	}
+
+	return nil
+}
