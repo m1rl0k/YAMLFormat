398f8893ed5cd649b083d5aa4c6701c975df95da
M1RL0K <11264689+m1rl0k@users.noreply.github.com>
2023-04-01 21:42:14 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,75 +1,72 @@
+package main
 
- package main
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 
- import (
-        "bytes"
- 	"encoding/json"
- 	"fmt"
- 	"io/ioutil"
- 	"os"
- 	"path/filepath"
- 	"strings"
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclwrite"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"gopkg.in/yaml.v3"
+)
 
- 	"github.com/hashicorp/hcl/v2"
- 	"github.com/hashicorp/hcl/v2/hclwrite"
- 	"gopkg.in/yaml.v3"
- 	"github.com/hashicorp/hcl/v2/hclsyntax"
- )
+func main() {
+	if err := filepath.Walk(".", processFile); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+}
 
- func main() {
- 	if err := filepath.Walk(".", processFile); err != nil {
- 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
- 		os.Exit(1)
- 	}
- }
+func processFile(path string, info os.FileInfo, err error) error {
+	if err != nil {
+		return err
+	}
 
- func processFile(path string, info os.FileInfo, err error) error {
- 	if err != nil {
- 		return err
- 	}
+	if info.IsDir() {
+		return nil
+	}
 
- 	if info.IsDir() {
- 		return nil
- 	}
+	ext := filepath.Ext(path)
+	if ext == ".yaml" || ext == ".yml" {
+		return formatYAMLFile(path)
+	} else if ext == ".hcl" {
+		return formatHCLFile(path)
+	} else if ext == ".tf" {
+		return formatTerraformFile(path)
+	}
 
- 	ext := filepath.Ext(path)
- 	if ext == ".yaml" || ext == ".yml" {
- 		return formatYAMLFile(path)
- 	} else if ext == ".hcl" {
- 		return formatHCLFile(path)
- 	} else if ext == ".tf" {
- 		return formatTerraformFile(path)
- 	}
+	return nil
+}
 
- 	return nil
- }
+func formatYAMLFile(path string) error {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
 
+	var yamlData interface{}
+	if err := yaml.Unmarshal(data, &yamlData); err != nil {
+		return err
+	}
 
- func formatYAMLFile(path string) error {
- 	data, err := ioutil.ReadFile(path)
- 	if err != nil {
- 		return err
- 	}
+	formattedData, err := yaml.Marshal(yamlData)
+	if err != nil {
+		return err
+	}
 
- 	var yamlData interface{}
- 	if err := yaml.Unmarshal(data, &yamlData); err != nil {
- 		return err
- 	}
+	if !strings.EqualFold(string(data), string(formattedData)) {
+		if err := ioutil.WriteFile(path, formattedData, 0644); err != nil {
+			return err
+		}
+		fmt.Printf("Formatted YAML file: %s\n", path)
+	}
 
- 	formattedData, err := yaml.Marshal(yamlData)
- 	if err != nil {
- 		return err
- 	}
-
- 	if !strings.EqualFold(string(data), string(formattedData)) {
- 		if err := ioutil.WriteFile(path, formattedData, 0644); err != nil {
- 			return err
- 		}
- 		fmt.Printf("Formatted YAML file: %s\n", path)
- 	}
-
- 	return nil
- }
+	return nil
+}
 
 func formatHCLFile(path string) error {
 	data, err := ioutil.ReadFile(path)
@@ -102,13 +99,13 @@ func formatTerraformFile(path string) error {
 		return err
 	}
 
-	file, diag := hcl.Parse(data)
+	file, diag := hclsyntax.ParseConfig(data, path, hcl.Pos{Line: 1, Column: 1})
 	if diag.HasErrors() {
-		return fmt.Errorf("Error parsing Terraform file: %s", diag.Error())
+		return diag
 	}
 
 	formattedData := hclwrite.NewEmptyFile()
-	_ = hclwrite.MergeFiles([]*hclwrite.File{formattedData, hclwrite.NewFileFromAst(file)})
+	_ = hclwrite.MergeFiles([]*hclwrite.File{formattedData, hclwrite.NewFileFromTokens(file)})
 	formattedBytes := formattedData.Bytes()
 
 	if !bytes.Equal(data, formattedBytes) {
@@ -120,4 +117,3 @@ func formatTerraformFile(path string) error {
 
 	return nil
 }
-
