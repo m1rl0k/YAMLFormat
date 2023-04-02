a08b7c7c56c723dd3ba2ea96c3ec2098bb81280d
M1RL0K <11264689+m1rl0k@users.noreply.github.com>
2023-04-01 22:37:02 -0400

    Update main.go

--- main.go
+++ main.go
@@ -7,9 +7,6 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclwrite"
-	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pmezard/go-difflib/difflib"
 	"gopkg.in/yaml.v3"
 )
@@ -33,8 +30,6 @@ func processFile(path string, info os.FileInfo, err error) error {
 	ext := filepath.Ext(path)
 	if ext == ".yaml" || ext == ".yml" {
 		return formatYAMLFile(path)
-	} else if ext == ".tf" {
-		return formatTerraformFile(path)
 	}
 
 	return nil
@@ -69,45 +64,4 @@ func formatYAMLFile(path string) error {
 	}
 
 	return nil
-}
-
-func formatTerraformFile(path string) error {
-	data, err := ioutil.ReadFile(path)
-	if err != nil {
-		return err
-	}
-
-	file, diags := hclsyntax.ParseConfig(data, path, hcl.Pos{Line: 1, Column: 1})
-	if diags.HasErrors() {
-		return fmt.Errorf("Error parsing Terraform file '%s': %s", path, diags.Error())
-	}
-
-	hclwriteFile := hclwrite.NewEmptyFile()
-	hclwriteGo(file.Body, hclwriteFile.Body())
-
-	formattedData := hclwrite.Format(hclwriteFile.Bytes())
-
-	if !strings.EqualFold(string(data), string(formattedData)) {
-		diff, _ := difflib.GetUnifiedDiffString(difflib.UnifiedDiff{
-			A:        difflib.SplitLines(string(data)),
-			B:        difflib.SplitLines(string(formattedData)),
-			FromFile: "Original",
-			ToFile:   "Formatted",
-			Context:  3,
-		})
-		fmt.Printf("Differences in Terraform file: %s\n", path)
-		fmt.Println(diff)
-	}
-
-	return nil
-}
-
-func hclwriteGo(src hcl.Body, dst *hclwrite.Body) {
-	for _, block := range src.Blocks() {
-		dst.AppendBlock(block.Type, block.Labels).SetBody(block.Body)
-	}
-
-	for name, attr := range src.Attributes() {
-		dst.SetAttribute(name, attr.Expr)
-	}
 }
