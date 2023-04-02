06782e0b1a55730c2ec6e6d3c58b8f8204e4b9ef
M1RL0K <11264689+m1rl0k@users.noreply.github.com>
2023-04-02 09:46:46 -0400

    Update tf.go

--- tf.go
+++ tf.go
@@ -9,7 +9,7 @@ import (
 	"github.com/fatih/color"
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclparse"
-	"github.com/hashicorp/hcl/v2/hclwrite"
+	"github.com/hashicorp/hcl/v2/hclwrite@latest"
 	"github.com/pmezard/go-difflib/difflib"
         
 
