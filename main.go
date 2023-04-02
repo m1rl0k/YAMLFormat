6efa5b679c6463189a02d3af711537b9d26728ce
M1RL0K <11264689+m1rl0k@users.noreply.github.com>
2023-04-02 19:02:14 -0400

    Update main.go

--- main.go
+++ main.go
@@ -77,6 +77,37 @@ func updateYAMLNodeStyle(node *yaml.Node) {
 	}
 }
 
+func suggestFixForLine(line string) string {
+	fixed := strings.TrimSpace(line)
+
+	if strings.HasPrefix(fixed, "-") {
+		// Fix misplaced hyphens for list items
+		hyphenIndex := strings.Index(line, "-")
+		if hyphenIndex > 0 {
+			fixed = strings.Repeat(" ", hyphenIndex) + "- " + strings.TrimSpace(fixed[1:])
+		} else {
+			fixed = "- " + strings.TrimSpace(fixed[1:])
+		}
+	} else if strings.Contains(fixed, ":") {
+		// Fix extra colons or incorrect indentation for key-value pairs
+		parts := strings.SplitN(fixed, ":", 2)
+		key := strings.TrimSpace(parts[0])
+		value := strings.TrimSpace(parts[1])
+
+		// If the value contains a colon, assume it's an error and remove it
+		if strings.Contains(value, ":") {
+			value = strings.Replace(value, ":", "", 1)
+		}
+
+		fixed = fmt.Sprintf("%s: %s", key, value)
+	} else {
+		// Assume a missing colon for other cases
+		fixed = fmt.Sprintf("%s:", fixed)
+	}
+
+	return fixed
+}
+
 func showDiff(path, original, formatted string) {
 	dmp := diffmatchpatch.New()
 	diffs := dmp.DiffMain(original, formatted, false)
