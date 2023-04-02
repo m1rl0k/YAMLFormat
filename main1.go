0ae79ec9606f647d5ca21da3928462cae126e6b9
M1RL0K <11264689+m1rl0k@users.noreply.github.com>
2023-04-01 23:55:48 -0400

    Update main1.go

--- main1.go
+++ main1.go
@@ -1,45 +1,5 @@
-package main
+package tf
 
-import (
-    "fmt"
-    "io/ioutil"
-    "os"
-    "path/filepath"
-    "strings"
-    
-    "github.com/hashicorp/hcl/v2/hclwrite"
-
-    "github.com/pmezard/go-difflib/difflib"
-    "github.com/hashicorp/hcl/v2/hclsyntax"
-)
-
-func main() {
-    dir, err := os.Getwd()
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-        os.Exit(1)
-    }
-
-    if err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            return err
-        }
-
-        if info.IsDir() {
-            return nil
-        }
-
-        ext := filepath.Ext(path)
-        if ext == ".tf" || ext == ".tfvars" {
-            return formatTerraformFile(path)
-        }
-
-        return nil
-    }); err != nil {
-        fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-        os.Exit(1)
-    }
-}
 
 func formatTerraformFile(path string) error {
     data, err := ioutil.ReadFile(path)
@@ -76,23 +36,4 @@ func formatTerraform(data []byte) ([]byte, error) {
 
     config := hclsyntax.EncodeConfig(file.Body, &hclwrite.Indent{})
     return config.Bytes(), nil
-}
-
-func formatDiff(diff string) string {
-    var formattedDiff strings.Builder
-
-    for _, line := range strings.Split(diff, "\n") {
-        switch {
-        case strings.HasPrefix(line, "+"):
-            formattedDiff.WriteString("\033[32m" + line + "\033[0m")
-        case strings.HasPrefix(line, "-"):
-            formattedDiff.WriteString("\033[31m" + line + "\033[0m")
-        default:
-            formattedDiff.WriteString(line)
-        }
-
-        formattedDiff.WriteString("\n")
-    }
-
-    return formattedDiff.String()
 }
