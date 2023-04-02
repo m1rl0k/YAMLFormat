b28ad15ec1e127e6e4f7802a26d6c1633b6e94de
M1RL0K <11264689+m1rl0k@users.noreply.github.com>
2023-04-01 20:30:17 -0400

    Update main.go

--- main.go
+++ main.go
@@ -34,10 +34,13 @@ func processFile(path string, info os.FileInfo, err error) error {
 		return formatYAMLFile(path)
 	} else if ext == ".hcl" {
 		return formatHCLFile(path)
+	} else if ext == ".tf" {
+		return formatTerraformFile(path)
 	}
 
 	return nil
 }
+
 
 func formatYAMLFile(path string) error {
 	data, err := ioutil.ReadFile(path)
@@ -97,4 +100,35 @@ func formatHCLFile(path string) error {
 
 	return nil
 }
+func formatTerraformFile(path string) error {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
 
+	file, diags := hclsyntax.ParseConfig(data, path, hcl.Pos{Line: 1, Column: 1})
+	if diags.HasErrors() {
+		return fmt.Errorf("Error parsing Terraform file '%s': %s", path, diags.Error())
+	}
+
+	jsonBytes, err := json.Marshal(file.Body())
+	if err != nil {
+		return err
+	}
+
+	hclwriteFile, err := hclwrite.ParseJSON(jsonBytes, path)
+	if err != nil {
+		return err
+	}
+
+	formattedData := hclwrite.Format(hclwriteFile.Bytes())
+
+	if !strings.EqualFold(string(data), string(formattedData)) {
+		if err := ioutil.WriteFile(path, formattedData, 0644); err != nil {
+			return err
+		}
+		fmt.Printf("Formatted Terraform file: %s\n", path)
+	}
+
+	return nil
+}
