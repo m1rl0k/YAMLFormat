466ed3a7273dc0e0382ac680b41ed35820389d55
M1RL0K <11264689+m1rl0k@users.noreply.github.com>
2023-04-02 19:41:16 -0400

    Update yaml.go

--- yaml.go
+++ yaml.go
@@ -10,28 +10,23 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/mattn/go-colorable"
 	"github.com/sergi/go-diff/diffmatchpatch"
 	"gopkg.in/yaml.v3"
 )
 
 func main() {
-	// Get the current working directory
 	wd, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Read and process all YAML files in the directory
 	err = filepath.Walk(wd, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-
 		if !info.IsDir() && (strings.HasSuffix(info.Name(), ".yaml") || strings.HasSuffix(info.Name(), ".yml")) {
 			processYAMLFile(path)
 		}
-
 		return nil
 	})
 
@@ -41,48 +36,48 @@ func main() {
 }
 
 func processYAMLFile(path string) {
-	// Read the YAML file
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
 		log.Printf("Error reading file %s: %v\n", path, err)
 		return
 	}
 
-	// Parse the YAML content
 	var parsedContent yaml.Node
 	err = yaml.Unmarshal(data, &parsedContent)
 	if err != nil {
-		// Find the line number, content, and suggested fix of the error
-		line, content, suggestedFix := findErrorLineAndSuggestFix(string(data), err)
-		log.Printf("Error parsing YAML in file %s (line %d: %q): %v\n", path, line, content, err)
-		log.Printf("Suggested fix for line %d: %q\n", line, suggestedFix)
+		log.Printf("Error parsing YAML in file %s: %v\n", path, err)
 		return
 	}
 
-	// Update YAML node style
 	updateYAMLNodeStyle(&parsedContent)
 
-	// Format the parsed YAML content
 	formatted, err := yaml.Marshal(&parsedContent)
 	if err != nil {
 		log.Printf("Error formatting YAML in file %s: %v\n", path, err)
 		return
 	}
 
-	// Generate and display the diff
 	showDiff(path, string(data), string(formatted))
+}
+
+func updateYAMLNodeStyle(node *yaml.Node) {
+	if node.Kind == yaml.MappingNode || node.Kind == yaml.SequenceNode {
+		node.Style = yaml.FlowStyle
+	}
+	for i := 0; i < len(node.Content); i++ {
+		updateYAMLNodeStyle(node.Content[i])
+	}
 }
 
 func findErrorLineAndSuggestFix(data string, err error) (int, string, string) {
 	line := -1
 
-	// Use a regex to extract the line number from the error message
 	re := regexp.MustCompile(`line (\d+):`)
 	matches := re.FindStringSubmatch(err.Error())
 	if len(matches) > 1 {
-		var convErr error
-		line, convErr = strconv.Atoi(matches[1])
-		if convErr != nil {
+		var err error
+		line, err = strconv.Atoi(matches[1])
+		if err != nil {
 			line = -1
 		}
 	}
@@ -95,68 +90,44 @@ func findErrorLineAndSuggestFix(data string, err error) (int, string, string) {
 	return -1, "", ""
 }
 
-func updateYAMLNodeStyle(node *yaml.Node) {
-	if node.Kind == yaml.MappingNode || node.Kind == yaml.SequenceNode {
-		node.Style = yaml.FlowStyle
-	}
-
-	for i := 0; i < len(node.Content); i++ {
-		updateYAMLNodeStyle(node.Content[i])
-	}
-}
-
 func showDiff(path, original, formatted string) {
 	dmp := diffmatchpatch.New()
 	diffs := dmp.DiffMain(original, formatted, false)
 
 	if len(diffs) > 1 {
 		fmt.Printf("Differences in file %s:\n", path)
-		coloredOutput := diffToColoredYAMLText(diffs)
-		colorable.NewColorableStdout().Write([]byte(coloredOutput))
-		fmt.Println()
-	}
-}
 
-func diffToColoredYAMLText(diffs []diffmatchpatch.Diff) string {
-	var output strings.Builder
-	for _, diff := range diffs {
-		text := diff.Text
+		deltas := dmp.DiffToDelta(diffs)
+		originalLines := strings.Split(original, "\n")
+		formattedLines := strings.Split(formatted, "\n")
 
-		if diff.Type == diffmatchpatch.DiffInsert {
-			// Green for inserted lines
-			output.WriteString("\x1b[32m")
-			output.WriteString(text)
-			output.WriteString("\x1b[0m")
-		} else if diff.Type == diffmatchpatch.DiffDelete {
-			// Red for deleted lines
-			output.WriteString("\x1b[31m")
-			output.WriteString(text)
-			output.WriteString("\x1b[0m")
-		} else {
-			output.WriteString(text)
+		pos := 0
+		for _, delta := range deltas {
+			if delta.Type == diffmatchpatch.DiffEqual {
+				pos += delta.Length1
+				continue
+			}
+
+			start := pos - 12
+			if start < 0 {
+				start = 0
+			}
+			end := pos + 12
+			if end >= len(originalLines) {
+				end = len(originalLines) - 1
+			}
+
+			fmt.Println("Original:")
+			for i := start; i <= end; i++ {
+				fmt.Printf("%4d: %s\n", i+1, originalLines[i])
+			}
+
+			fmt.Println("Formatted:")
+			for i := start; i <= end; i++ {
+				fmt.Printf("%4d: %s\n", i+1, formattedLines[i])
+			}
+
+			pos += delta.Length1
 		}
 	}
-	return output.String()
-}
-
-func suggestFixForLine(line string) string {
-	fixed := strings.TrimSpace(line)
-
-	if strings.Contains(fixed, ":") {
-		parts := strings.SplitN(fixed, ":", 2)
-		key := strings.TrimSpace(parts[0])
-		value := strings.TrimSpace(parts[1])
-
-		// If the value contains a colon, assume it's an error and remove it
-		if strings.Contains(value, ":") {
-			value = strings.Replace(value, ":", "", 1)
-		}
-
-		fixed = fmt.Sprintf("%s: %s", key, value)
-	} else {
-		// If there's no colon, assume it's missing and add one
-		fixed = fmt.Sprintf("%s:", fixed)
-	}
-
-	return fixed
 }
