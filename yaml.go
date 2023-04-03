aa854ad5595354213d4d4363fb5bd558b5ec217d
M1RL0K <11264689+m1rl0k@users.noreply.github.com>
2023-04-02 20:05:39 -0400

    Update yaml.go

--- yaml.go
+++ yaml.go
@@ -1,96 +1,3 @@
-package main
-
-import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
-	"path/filepath"
-	"regexp"
-	"strconv"
-	"strings"
-        "unicode"
-
-	"github.com/sergi/go-diff/diffmatchpatch"
-	"gopkg.in/yaml.v3"
-)
-
-func main() {
-	wd, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = filepath.Walk(wd, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if !info.IsDir() && (strings.HasSuffix(info.Name(), ".yaml") || strings.HasSuffix(info.Name(), ".yml")) {
-			processYAMLFile(path)
-		}
-		return nil
-	})
-
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func processYAMLFile(path string) {
-	data, err := ioutil.ReadFile(path)
-	if err != nil {
-		log.Printf("Error reading file %s: %v\n", path, err)
-		return
-	}
-
-	var parsedContent yaml.Node
-	err = yaml.Unmarshal(data, &parsedContent)
-	if err != nil {
-		log.Printf("Error parsing YAML in file %s: %v\n", path, err)
-		return
-	}
-
-	updateYAMLNodeStyle(&parsedContent)
-
-	formatted, err := yaml.Marshal(&parsedContent)
-	if err != nil {
-		log.Printf("Error formatting YAML in file %s: %v\n", path, err)
-		return
-	}
-
-	showDiff(path, string(data), string(formatted))
-}
-
-func updateYAMLNodeStyle(node *yaml.Node) {
-	if node.Kind == yaml.MappingNode || node.Kind == yaml.SequenceNode {
-		node.Style = yaml.FlowStyle
-	}
-	for i := 0; i < len(node.Content); i++ {
-		updateYAMLNodeStyle(node.Content[i])
-	}
-}
-
-func findErrorLineAndSuggestFix(data string, err error) (int, string, string) {
-	line := -1
-
-	re := regexp.MustCompile(`line (\d+):`)
-	matches := re.FindStringSubmatch(err.Error())
-	if len(matches) > 1 {
-		var err error
-		line, err = strconv.Atoi(matches[1])
-		if err != nil {
-			line = -1
-		}
-	}
-
-	lines := strings.Split(data, "\n")
-	if line > 0 && line <= len(lines) {
-		return line, lines[line-1], suggestFixForLine(lines[line-1])
-	}
-
-	return -1, "", ""
-}
-
 func showDiff(path, original, formatted string) {
     dmp := diffmatchpatch.New()
     diffs := dmp.DiffMain(original, formatted, false)
@@ -141,7 +48,6 @@ func showDiff(path, original, formatted string) {
         }
     }
 }
-
 
 
 func suggestFixForLine(line string) string {
