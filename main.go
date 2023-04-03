c866d8f8b44d6aa2445bf33d1a75a6a055940c67
M1RL0K <11264689+m1rl0k@users.noreply.github.com>
2023-04-02 20:41:52 -0400

    Update main.go

--- main.go
+++ main.go
@@ -6,31 +6,28 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
-        "regexp"
+	"regexp"
 	"strconv"
+	"strings"
+	"unicode"
 
 	"github.com/sergi/go-diff/diffmatchpatch"
 	"gopkg.in/yaml.v3"
 )
 
 func main() {
-	// Get the current working directory
 	wd, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Read and process all YAML files in the directory
 	err = filepath.Walk(wd, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-
 		if !info.IsDir() && (strings.HasSuffix(info.Name(), ".yaml") || strings.HasSuffix(info.Name(), ".yml")) {
 			processYAMLFile(path)
 		}
-
 		return nil
 	})
 
@@ -40,14 +37,12 @@ func main() {
 }
 
 func processYAMLFile(path string) {
-	// Read the YAML file
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
 		log.Printf("Error reading file %s: %v\n", path, err)
 		return
 	}
 
-	// Parse the YAML content
 	var parsedContent yaml.Node
 	err = yaml.Unmarshal(data, &parsedContent)
 	if err != nil {
@@ -55,17 +50,14 @@ func processYAMLFile(path string) {
 		return
 	}
 
-	// Update YAML node style
 	updateYAMLNodeStyle(&parsedContent)
 
-	// Format the parsed YAML content
 	formatted, err := yaml.Marshal(&parsedContent)
 	if err != nil {
 		log.Printf("Error formatting YAML in file %s: %v\n", path, err)
 		return
 	}
 
-	// Generate and display the diff
 	showDiff(path, string(data), string(formatted))
 }
 
@@ -73,7 +65,6 @@ func updateYAMLNodeStyle(node *yaml.Node) {
 	if node.Kind == yaml.MappingNode || node.Kind == yaml.SequenceNode {
 		node.Style = yaml.FlowStyle
 	}
-
 	for i := 0; i < len(node.Content); i++ {
 		updateYAMLNodeStyle(node.Content[i])
 	}
@@ -82,7 +73,6 @@ func updateYAMLNodeStyle(node *yaml.Node) {
 func findErrorLineAndSuggestFix(data string, err error) (int, string, string) {
 	line := -1
 
-	// Use a regex to extract the line number from the error message
 	re := regexp.MustCompile(`line (\d+):`)
 	matches := re.FindStringSubmatch(err.Error())
 	if len(matches) > 1 {
@@ -109,54 +99,47 @@ func showDiff(path, original, formatted string) {
 		fmt.Printf("Differences in file %s:\n", path)
 
 		deltas := dmp.DiffToDelta(diffs)
-		originalLines := strings.Split(original, "\n")
-		formattedLines := strings.Split(formatted, "\n")
-
 		pos := 0
 		for _, delta := range deltas {
-			if delta.Type == diffmatchpatch.DiffEqual {
-				pos += delta.Length1
-				continue
-			}
+			deltaType := string(delta[0])
 
-			start := pos - 12
-			if start < 0 {
-				start = 0
-			}
-			end := pos + 12
-			if end >= len(originalLines) {
-				end = len(originalLines) - 1
-			}
+			switch deltaType {
+			case "+":
+				length, _ := strconv.Atoi(delta[1:])
+				fmt.Println("Added:")
+				fmt.Println(delta[2:])
+				pos += length
+			case "-":
+				length, _ := strconv.Atoi(delta[1:])
+				fmt.Println("Removed:")
+				start := pos - 12
+				if start < 0 {
+					start = 0
+				}
+				end := pos + length + 12
+				if end >= len(original) {
+					end = len(original) - 1
+				}
 
-			fmt.Println("Original:")
-			for i := start; i <= end; i++ {
-				fmt.Printf("%4d: %s\n", i+1, originalLines[i])
-			}
+				fmt.Println("Original:")
+				for i := start; i <= end; i++ {
+					fmt.Printf("%4d: %s\n", i+1, string(original[i]))
+				}
 
-			fmt.Println("Formatted:")
-			for i := start; i <= end; i++ {
-				fmt.Printf("%4d: %s\n", i+1, formattedLines[i])
-			}
+				fmt.Println("Formatted:")
+				for i := start; i <= end; i++ {
+					fmt.Printf("%4d: %s\n", i+1, string(formatted[i]))
+				}
 
-			pos += delta.Length1
+				pos += length
+			case "=":
+				length, _ := strconv.Atoi(delta[1:])
+				pos += length
+			}
 		}
 	}
 }
 
-func findErrorLineAndSuggestFix(data string, err error) (int, string, string) {
-	var line, column int
-
-	if syntaxErr, ok := err.(*yaml.SyntaxError); ok {
-		line, column = syntaxErr.Line, syntaxErr.Column
-	}
-
-	lines := strings.Split(data, "\n")
-	if line > 0 && line <= len(lines) {
-		return line, lines[line-1], suggestFixForLine(lines[line-1])
-	}
-
-	return -1, "", ""
-}
 func suggestFixForLine(line string) string {
 	fixedLine := line
 
